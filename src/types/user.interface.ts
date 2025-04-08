d0063045d25e74b207172ffdbef45ab959895c16
jaeyo03 <toyazx@naver.com>
2025-04-08 17:50:55 +0900

    feat : 선박 관련 인터페이스 추가

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -20,3 +20,37 @@ export interface CaptainUser extends BaseUser {
 }
 
 export type User = NormalUser | CaptainUser
+
+export interface BoatInputData {
+  nickname : string;
+  fileId? : number;
+  description : string;
+  shipLicenseNumber : string;
+  shipList : number[];
+}
+
+export interface BoatInfo {
+  shipName: string;
+  shipNumber: string;
+  departurePort: string;
+  passengerCapacity: string;
+  restroomType : 'PUBLIC' | 'SEPARATION' | 'NONE';
+  loungeArea : boolean;
+  kitchenFacility : boolean;
+  fishingChair : boolean;
+  passengerInsurance : boolean;
+  fishingGearRental : boolean;
+  mealProvided : boolean;
+  parkingAvailable : boolean;
+}
+
+export interface BoatData extends BoatInfo{
+  id : number;
+  isSaved : boolean;
+}
+
+export type BooleanKeys<T> = {
+  [K in keyof T]: T[K] extends boolean ? K : never;
+}[keyof T];
+
+export type BoatInfoBooleanKeys = BooleanKeys<BoatData>;
