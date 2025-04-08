0fb29d3941b55e54884d8c63aa027b65e839c87a
jaeyo03 <toyazx@naver.com>
2025-04-08 17:50:45 +0900

    feat : 선박 정보 추가 및 선장 정보 추가 api 구현

--- src/lib/api/userAPI.ts
+++ src/lib/api/userAPI.ts
@@ -1,5 +1,5 @@
-import axios, {InternalAxiosRequestConfig} from "axios";
-import {User} from "@/types/user.interface";
+import {BoatInfo, BoatInputData, User} from "@/types/user.interface";
+import {apiInstance} from "@/lib/api/apiInstance";
 
 // 백엔드가 설정한 기본 API 응답
 interface APIDataResponse<T> {
@@ -7,21 +7,6 @@ interface APIDataResponse<T> {
   success : boolean;
   timestamp : string;
 }
-
-export const apiInstance = axios.create({
-  baseURL: 'https://api.mikki.kr/api/v1',
-  timeout: 5000,
-})
-
-apiInstance.interceptors.request.use(
-  function getAccessToken(config : InternalAxiosRequestConfig) : InternalAxiosRequestConfig {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      config.headers.Authorization = token
-    }
-    return config;
-  }
-)
 
 export class UserAPI {
   public async getMemberInfo() : Promise<User | null> {
@@ -46,13 +31,28 @@ export class UserAPI {
 
   }
 
-  public async postCaptainMemberInfo(data : unknown) {
-
+  public async postCaptainMemberInfo(boatInput : BoatInputData) : Promise<APIDataResponse<unknown> | null> {
+    try {
+      const response = await apiInstance.post('/members/captains',boatInput);
+      return response.data
+    } catch (error) {
+      console.error('getMemberInfo error:', error)
+      return null
+    }
   }
 
   public async patchCaptainMemberInfo(data : unknown) {
 
   }
 
+  public async postCaptainBoatInfo(boat : BoatInfo) {
+    try {
+      const response = await apiInstance.post('/ship',boat);
+      return response.headers
+    } catch (error) {
+      console.error('getMemberInfo error:', error)
+      return null
+    }
+  }
 }
 
