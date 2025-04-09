b16839823217e755cc5ccd1680775f1f56c8d5f7
jaeyo03 <toyazx@naver.com>
2025-04-09 17:50:06 +0900

    feat : 물고기 관련 타입 추가

--- src/types/fish.interface.ts
+++ src/types/fish.interface.ts
@@ -1,6 +1,34 @@
 export interface FishInfo {
-  image: string
-  fishName: string
-  count: number
-  largestSize: number
+  fishEncyclopediaId : number;
+  fileUrl: string;
+  fishName: string;
+  bestLength: number | null;
+  totalCount: number | null;
+}
+
+export interface FishDetailInfo {
+  fishEncyclopediaId : number;
+  length : number;
+  count : number;
+  fishPointName : string;
+  fishPointDetailName : string;
+  createdAt : string;
+}
+
+export interface FishUpload {
+  fishId : number;
+  length : number;
+  count : number;
+  fishPointId : number;
+}
+
+type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+export interface FishDictionaryInfo {
+  fishId : number;
+  name : string;
+  description : string;
+  fileUrl : string;
+  spawnSeasonList : Month[];
+  spawnLocation : string;
 }
