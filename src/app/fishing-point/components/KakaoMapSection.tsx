13e86ffc0cb65656b0881ad0664327b623562e98
kim dong hyeon <kdh990315@naver.com>
2025-04-09 15:23:31 +0900

    feat: 지역 select 컴포넌트 추가

--- src/app/fishing-point/components/KakaoMapSection.tsx
+++ src/app/fishing-point/components/KakaoMapSection.tsx
@@ -1,14 +1,42 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import KaKaoMap from "./KaKaoMap";
+import { FishingPointLocation } from "@/types/fishingPointLocationType";
 
-export default function KakaoMapSection() {
+export default function KakaoMapSection({
+  locationData,
+}: {
+  locationData: FishingPointLocation[];
+}) {
   return (
     <section className="w-full mt-[34px]">
       <div className="xl:w-[1280px] w-[calc(100%-20px)] mx-auto">
         <div className="flex items-center justify-between mb-[16px]">
-          <div>서치바</div>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="지역을 선택해주세요" />
+            </SelectTrigger>
+            <SelectContent
+              side="bottom"
+              sideOffset={4}
+              align="start"
+              avoidCollisions={false}
+            >
+              {locationData.map((location) => (
+                <SelectItem key={location.regionId} value={location.regionId}>
+                  {location.regionName}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
           <Button className="text-body-3 text-white">
             낚시 포인트 제보하기
           </Button>
