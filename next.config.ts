d5c1a7cafa9fd7b7738cb2bb9ca38495ee2307c9
jaeyo03 <toyazx@naver.com>
2025-04-09 17:44:21 +0900

    chore : s3 이미지 설정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'devcouse4-team13-bucket.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/post/**',
+        search: '',
+      },
+    ],
+  },
   /* config options here */
 };
 
