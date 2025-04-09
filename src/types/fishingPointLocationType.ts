5099b21f7bb0ecde97bb8de304e13a47f8cdce33
kim dong hyeon <kdh990315@naver.com>
2025-04-10 04:27:54 +0900

    feat: 낚시 상세정보 api 타입정의 및 api endpoint정의

--- src/types/fishingPointLocationType.ts
+++ src/types/fishingPointLocationType.ts
@@ -2,3 +2,28 @@ export interface FishingPointLocation {
   regionId: string;
   regionName: string;
 }
+
+export interface FishingPoint {
+  fishPointId: number;
+  fishPointName: string;
+  fishPointDetailName: string;
+  latitude: number;
+  longitude: number;
+  isBan: boolean;
+}
+
+export interface FishingPointDetail {
+  fishPointId: number;
+  fishPointName: string;
+  fishPointDetailName: string;
+  latitude: number;
+  longitude: number;
+  isBan: boolean;
+  fishList: [];
+}
+
+export interface FishingPointDetailAPIResponse {
+  timestamp: string;
+  data: FishingPointDetail;
+  success: boolean;
+}
