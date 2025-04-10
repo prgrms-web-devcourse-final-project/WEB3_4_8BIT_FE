3efa129e3669b7201b647100f278fac3a3216f14
jaeyo03 <toyazx@naver.com>
2025-04-10 15:12:06 +0900

    refactor : 안 쓰는 import 삭제

--- src/app/user/mypage/fish-encyclopedia/components/FishRegisterModal.tsx
+++ src/app/user/mypage/fish-encyclopedia/components/FishRegisterModal.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useReducer, useState} from "react";
+import React, {useReducer, useState} from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {Button} from "@/components/ui/button";
