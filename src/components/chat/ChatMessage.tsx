5a37d7ba02605f6b6810301083e3d7d53fe83716
vdvhk12 <vdvhk12@gmail.com>
2025-04-15 02:44:00 +0900

    refactor: 로컬 스토리지에서 회원 ID 가져와서 발신자 확인 (임시) (MIKKI-233)

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -26,11 +26,16 @@ export function ChatMessage({ message }: { message: ChatMessageResponse }) {
     }
   };
   const formattedTime = getFormattedTime();
+  const userData = JSON.parse(localStorage.getItem("user-storage") || "{}");
+  const memberId = userData.state?.user?.memberId || null;
+  const isOwn = message.senderId === memberId;
 
   // TODO 이미지 눌렀을때 크게 나오도록 하기?
   // TODO 추후 id 3이랑 비교하는 부분 다 바꿔야함! message.senderId === 3 => isOwn 으로
+  // TODO isOwn으로 발신자 구분하면 이전 메세지는 구분이가능한데, 웹소켓으로 보낸 메세지는 서버쪽에서 별도로 메세지를 전송해야함.
+  // TODO 그래서 일단 memberId로 발신자 구분으로 수정
   return (
-    <div className={`group flex items-start font-medium gap-3 ${message.isOwn ? "flex-row-reverse" : ""}`}>
+    <div className={`group flex items-start font-medium gap-3 ${isOwn ? "flex-row-reverse" : ""}`}>
       <div className="relative flex-shrink-0">
         <Avatar>
           <AvatarImage src={message.senderProfileImageUrl} alt={message.senderNickname} />
@@ -38,15 +43,15 @@ export function ChatMessage({ message }: { message: ChatMessageResponse }) {
         </Avatar>
       </div>
 
-      <div className={`flex flex-col ${message.isOwn ? "items-end" : "items-start"} max-w-[75%]`}>
+      <div className={`flex flex-col ${isOwn ? "items-end" : "items-start"} max-w-[75%]`}>
         <div className="flex items-center gap-2 mb-1">
-          <span className={`text-sm font-medium ${message.isOwn ? "order-2" : ""}`}>{message.senderNickname}</span>
-          <span className={`text-xs text-gray-40 ${message.isOwn ? "order-1" : ""}`}>{formattedTime}</span>
+          <span className={`text-sm font-medium ${isOwn ? "order-2" : ""}`}>{message.senderNickname}</span>
+          <span className={`text-xs text-gray-40 ${isOwn ? "order-1" : ""}`}>{formattedTime}</span>
         </div>
 
         <div
           className={`rounded-lg px-4 py-2 ${
-            message.isOwn ? "bg-primary text-white rounded-tr-none" : "bg-gray-80 text-gray-10"
+            isOwn ? "bg-primary text-white rounded-tr-none" : "bg-gray-80 text-gray-10"
           }`}
         >
           <p className="whitespace-pre-wrap">{message.content}</p>
